9ae824bb1d5669289d1816c9aef29a1b7c5aa7bd
pttransamdriver <entrepreneurracer.gmail.com>
2023-02-25 15:36:28 -0900

    reviewing dates

--- Notes_Data_and_date.py
+++ Notes_Data_and_date.py
@@ -8,11 +8,25 @@ print(current_date)
 # To make this cleaner you can import only a portion of the datetime library: 
 from datetime import datetime , timedelta
 # Then your varable definition will be simpler
-current_date2 = datetime.now()
+today = datetime.now()
 yesterday = datetime.today() - timedelta(days=1)
+next_week = datetime.today() + timedelta(weeks=1)
 # and print the same thing
-print(current_date2)
+print(today)
 # Let's stack some of out learning from the Variables and Strings lessons and add the 'f' operator
-print(f"The current date is {current_date2}")
+print(f"The current date is {current_date}")
+print(f"The current date is {today}")
 print(f"Yesterday was: {str(yesterday)}")
-print(f"{str(current_date.month)}/{str(current_date.day)}/{str(current_date.year)}")
+print(f"Next week is: {str(next_week)}")
+print(f"The more readable today: {str(current_date.month)}/{str(current_date.day)}/{str(current_date.year)}")
+print(f"The more readable Next Week: {str(next_week.month)}/{str(next_week.day)}/{str(next_week.year)}")
+
+
+# Let's do something with this. Ask the user for a birthday and format it
+bday = input("when is your birthday in mm/dd/yyyy?: ")
+# Now use the 'strip time' function to pull the Month, Day and Year out of the sring so we can manipulate it with the datetime function
+bday_date = datetime.strptime(bday, "%m/%d/%Y")
+print(f"Your Birthday is {bday_date}")
+bday_eve = bday_date - timedelta(days=1)
+print(f"The day before your birthday is {bday_eve}")
+
