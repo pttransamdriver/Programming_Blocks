39bcb15ff31120ba3b8d22ef59971a60394a7244
pttransamdriver <entrepreneurracer.gmail.com>
2023-03-02 22:11:47 -0900

    Fixing some of the ordering to make it easier to read

--- Group_Project-09.py
+++ Group_Project-09.py
@@ -27,21 +27,12 @@ print("The numbers you entered are:")
 for number in numbers:
     print(number)
 
-# Set the average variable by using the sum() divided by len() functions in python.  
-average = sum(numbers) / len(numbers)
-# Set the Sort variable from the sorted function
-sorted = sorted(numbers)
-
 # Find the smallest positive number and set that to 'positive_numbers' by using the
 # LIST COMPREHENSION feature in Python. This takes a list and evaluates 
 # This works by saying I want a new list called 'positive_numbers' 
 # To build this list use the number (first x) for every position (second 'x') in the list called
 # 'numbers' only if that number is greater than 0 (third x, 'if x>0' part)
 positive_numbers = [x for x in numbers if x > 0]
-
-# Printing the sum and averages
-print(f"The sum of the numbers you entered is {sum(numbers)}")
-print(f"The Average of the numbers you entered is {(average)}")
 
 # If Else statment for positive and negative numbers
 if positive_numbers:
@@ -50,4 +41,14 @@ if positive_numbers:
     print(f"The smallest positive number is {smallest_positive}")
 else:
     print("There are no positive numbers in the list.")
+
+
+# Set the average variable by using the sum() divided by len() functions in python.  
+average = sum(numbers) / len(numbers)
+# Set the Sort variable from the sorted function
+sorted = sorted(numbers)
+
+# Printing the sorted, sum and averages listseses
 print(f"Sorted numbers list is: {sorted}")
+print(f"The sum of the numbers you entered is {sum(numbers)}")
+print(f"The Average of the numbers you entered is {(average)}")
